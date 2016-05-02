3f2d4ec6d0d76b6dd7921db1b89355be4410906a
Yao Adzaku <yao@abakhi.com>
2016-05-02 15:15:46 +0000

    Make eval lexically scoped.
    Also add map, reduce

--- lib/plex.ex
+++ lib/plex.ex
@@ -31,12 +31,10 @@ defmodule Plex do
               fst: &(elem(&1, 0)),
               snd: &(elem(&1, 1)),
               nth: &Enum.at/2,
-              fail: &fail/1,
+              fail: &fail!/1,
               _argv_: &System.argv/0,
               print: &IO.puts/1,
               read: &IO.gets/1,
-              map: &Enum.map/3,
-              reduce: &Enum.reduce/3,
             head: fn [h|_] -> h end,
             tail: fn [_|t] -> t end
           }
@@ -45,14 +43,32 @@ defmodule Plex do
     def bootstrap(env) do
       Env.merge(env, namespace)
 
-      Env.bind(env, :eval, fn code -> Compiler.eval!(code, env) end)
+      Env.bind(env, :eval, fn code ->
+        local_scope = Env.new(env)
+        Compiler.eval!(code, local_scope)
+      end)
 
+      Env.bind(env, :map, fn seq, fun ->
+        Enum.map(seq, fn elem ->
+          fun.value.([elem])
+        end)
+      end)
+
+      Env.bind(env, :reduce, fn seq, acc, fun ->
+        Enum.reduce(seq, acc, fn elem, acc ->
+          fun.value.([elem, acc])
+        end)
+      end)
+
+      # now return the env
       env
     end
 
-    def fail(message) do
-      raise Plex.Compiler.RuntimeError, message: message
+    def fail!(message) do
+      raise Compiler.RuntimeError, message: message
     end
 
+    def reduce(xs, fun) do
+    end
   end
 end
